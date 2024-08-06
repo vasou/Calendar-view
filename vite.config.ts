b5525d1d3feb075c450222d212683ff9041c5e87
vasu <vasu@mindwaveventures.com>
2024-08-06 21:59:58 +0530

    Changes to Vite configuration for build

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  root: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true, // also necessary
+  },
 });
